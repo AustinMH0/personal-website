0ff8291db9b4cc05f9ee4a36a2ddb3b798d627bd
Austin Hendricks <austinhendricks@nevada.unr.edu>
2024-07-10 12:03:03 -0700

    In Main.jsx: Added animated heading, added social media icons w/ links, cleaned up formatting

--- my-react-app/src/components/Main.jsx
+++ my-react-app/src/components/Main.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+import { TypeAnimation } from 'react-type-animation';
 
 const Main = () => {
   return (
     <div id='main'>
-        <img className='' src='/lava_background.jpg'></img>
+        <img 
+        className='w-full h-screen object-cover object-left' 
+        src='/lava_background.jpg'
+        alt='background image depicting lava blobs'>
+        </img>
+        <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
+        <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
+          <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Austin Hendricks</h1>
+          <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>
+            I'm 
+            <TypeAnimation
+              sequence={[
+                'a Developer',
+                2000, 
+                'an Engineer',
+                2000,
+                'an Event Organizer',
+                2000,
+              ]}
+              wrapper='div'
+              speed={50}
+              style={{ fontSize: '1em', paddingLeft: '5px' }}
+              repeat={Infinity}
+            />
+          </h2>
+          <div className='flex justify-between pt-6 max-w-[75px] w-full'>
+            <a href='https://www.instagram.com/renderloop_/'>
+              <FaInstagram  className='cursor-pointer' size={20} />
+            </a>
+            <a href='https://www.linkedin.com/'>
+              <FaLinkedinIn className='cursor-pointer' size={20} />
+            </a>
+          </div>
+        </div>
+
+        </div>
     </div>
   )
 }
