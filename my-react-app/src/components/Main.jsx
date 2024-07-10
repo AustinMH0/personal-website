f793cb9be46b66d07696da1e7aee56000a0f8da6
Austin Hendricks <austinhendricks@nevada.unr.edu>
2024-07-09 18:03:44 -0700

    Started over with simpler design

---
+++ my-react-app/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div id='main'>
+        <img className='' src='/lava_background.jpg'></img>
+    </div>
+  )
+}
+
+export default Main 
