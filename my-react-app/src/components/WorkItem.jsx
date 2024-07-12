6287fb76e62d19e747659560046680e68cfae153
Austin Hendricks <austinhendricks@nevada.unr.edu>
2024-07-12 11:52:18 -0700

    Stylized details in Work.jsx to appear as an unordered list

--- my-react-app/src/components/WorkItem.jsx
+++ my-react-app/src/components/WorkItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const WorkItem = ({year, title, duration, details}) => {
+const WorkItem = ({year, title, duration, description, details}) => {
   return (
     <ol className='flex flex-col md:flex-row relative border-l border-stone-200'>
         <li className='mb-10 ml-4'>
@@ -10,7 +10,17 @@ const WorkItem = ({year, title, duration, details}) => {
                 <span className='text-lg font-semibold text-[#c531c8]'>{title}</span>
                 <span className='my-1 text-sm font-normal leading-none text-stone-300'>{duration}</span>
             </p>
-            <p className='my-2 text-base font-normal text-stone-300'>{details}</p>
+            <p className='my-2 text-base font-normal text-stone-300'>{description}</p>
+            {/* <div class="mx-auto flex min-h-screen max-w-screen-sm items-center justify-center">
+              <div class="h-36 w-full rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
+                <div class="flex h-full w-full items-center justify-center bg-gray-800 back">
+                  <h1 class="text-2xl font-black text-white">the quick brown fox jumps over the lazy dog</h1>
+                </div>
+              </div>
+            </div> */}
+            <ul className='my-2 text-base font-normal text-stone-300'>
+              <li>{details}</li>
+            </ul>
         </li>
     </ol>
   )
